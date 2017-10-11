17c033e431e6a6a359af0920343a5feca4d14f44
Roman <mail@romanbrazhnikov.ru>
2017-10-11 12:35:25 +0700

    MainActivity as session list. Material design dimens + color. Item & screen layout use resources

--- app/src/main/java/ru/romanbrazhnikov/simplefitnessdiary/views/MainActivity.java
+++ app/src/main/java/ru/romanbrazhnikov/simplefitnessdiary/views/MainActivity.java
@@ -17,19 +17,21 @@ import io.objectbox.Box;
 import io.objectbox.query.Query;
 import ru.romanbrazhnikov.simplefitnessdiary.R;
 import ru.romanbrazhnikov.simplefitnessdiary.base.views.BaseRecyclerViewActivity;
+import ru.romanbrazhnikov.simplefitnessdiary.entities.TrainingSession;
 import ru.romanbrazhnikov.simplefitnessdiary.entities.TrainingSet;
 
-public class MainActivity extends BaseRecyclerViewActivity<TrainingSet, TrainingSetViewHolder> {
+public class MainActivity extends BaseRecyclerViewActivity<TrainingSession, TrainingSessionViewHolder> {
     // FIELDS
     @Inject
     Box<TrainingSet> mTrainingSetBox;
 
+    @Inject
+    Box<TrainingSession> mTrainingSessionBox;
+
     // WIDGETS
-    @BindView(R.id.fab_addTrainingSet)
+    @BindView(R.id.fab_addTrainingSession)
     FloatingActionButton fab_addTrainingSet;
 
-    @BindView(R.id.b_testSession)
-    Button bTestSession;
 
     // LISTENERS
     AddClickListener mAddClickListener = new AddClickListener();
@@ -42,25 +44,14 @@ public class MainActivity extends BaseRecyclerViewActivity<TrainingSet, Training
         ButterKnife.bind(this);
         initListeners();
 
-        TrainingSet testSet = new TrainingSet();
-        testSet.setDate(new Date());
-        testSet.setExerciseType("Pull-ups");
-        testSet.setMeasurement("15 times 1");
-        testSet.setSessionId(1);
-        mTrainingSetBox.put(testSet);
+        TrainingSession session1 = new TrainingSession(new Date(), "Session 1 (" + Math.random() + ")");
+        TrainingSession session2 = new TrainingSession(new Date(), "Session 2 (" + Math.random() + ")");
+        mTrainingSessionBox.put(session1);
+        mTrainingSessionBox.put(session2);
 
-        TrainingSet testSet2 = new TrainingSet();
-        testSet2.setDate(new Date());
-        testSet2.setExerciseType("Push-ups");
-        testSet2.setMeasurement("30 times 2");
-        testSet2.setSessionId(2);
-        mTrainingSetBox.put(testSet2);
+
     }
 
-    @OnClick(R.id.b_testSession)
-    void testSession() {
-        TrainingSessionActivity.showNewActivity(this, 1L);
-    }
 
     private void initListeners() {
         fab_addTrainingSet.setOnClickListener(mAddClickListener);
@@ -79,28 +70,28 @@ public class MainActivity extends BaseRecyclerViewActivity<TrainingSet, Training
 
     private void updateUI() {
         // getting items and refreshing list
-        Query<TrainingSet> queryAll
-                = mTrainingSetBox.query()
+        Query<TrainingSession> queryAll
+                = mTrainingSessionBox.query()
                 .build();
 
-        List<TrainingSet> filteredRecords = queryAll.find();
+        List<TrainingSession> filteredRecords = queryAll.find();
 
         refreshList(filteredRecords);
     }
 
     @Override
     protected int getRecyclerViewID() {
-        return R.id.rv_trainingSets;
+        return R.id.rv_trainingSessions;
     }
 
     @Override
     protected int getItemLayoutID() {
-        return R.layout.item_training_set;
+        return R.layout.item_training_session;
     }
 
     @Override
-    protected TrainingSetViewHolder newViewHolder(View itemView) {
-        return new TrainingSetViewHolder(itemView, this);
+    protected TrainingSessionViewHolder newViewHolder(View itemView) {
+        return new TrainingSessionViewHolder(itemView, this);
     }
 
     @Override
